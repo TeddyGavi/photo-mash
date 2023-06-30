c8b78cb897d4539e01c3c4d9c2edff2ae805a3ab
TeddyGavi <matc.davis@gmail.com>
2023-06-30 16:00:10 -0700

    update types in camera screen, need to build out camera to take up entire screen

--- screens/CameraRoll.tsx
+++ screens/CameraRoll.tsx
@@ -1,19 +1,23 @@
 import { StyleSheet, View } from "react-native";
 import { Button, Text } from "react-native-paper";
 import React, { useEffect, useRef, useState } from "react";
-import { Camera } from "expo-camera";
+import { Camera, CameraCapturedPicture } from "expo-camera";
 import { shareAsync } from "expo-sharing";
 import * as MediaLibrary from "expo-media-library";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { Image } from "react-native";
+
 const CameraRoll = ({}) => {
-  const cameraRef = useRef();
+  const cameraRef = useRef<Camera>(null);
   const [hasCameraPermission, setHasCameraPermission] =
     useState<boolean>(false);
   const [hasMediaLibraryPermission, setHasMediaLibraryPermission] =
     useState<boolean>(false);
+  const [photo, setPhoto] = useState<CameraCapturedPicture>();
 
   useEffect(() => {
-    async () => {
+    (async () => {
       const cameraPermission = await Camera.requestCameraPermissionsAsync();
       const mediaLibraryPermissions =
         await MediaLibrary.requestPermissionsAsync();
@@ -21,7 +25,7 @@ const CameraRoll = ({}) => {
       setHasMediaLibraryPermission(
         mediaLibraryPermissions.status === "granted"
       );
-    };
+    })();
   }, []);
 
   if (!hasCameraPermission) {
@@ -34,16 +38,52 @@ const CameraRoll = ({}) => {
     );
   }
 
+  let takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false,
+    };
+
+    let newPhoto = await cameraRef.current?.takePictureAsync(options);
+    setPhoto(newPhoto);
+  };
+
+  if (photo) {
+    let sharePic = () => {
+      shareAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    let savePhoto = () => {
+      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image
+          style={styles.preview}
+          source={{ uri: "data:image/jpg;base64," + photo.base64 }}
+        />
+        <Button onPress={sharePic}>Share</Button>
+        {hasMediaLibraryPermission ? (
+          <Button onPress={savePhoto}>Save</Button>
+        ) : undefined}
+        <Button onPress={() => setPhoto(undefined)}>Discard</Button>
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <SafeAreaView>
-      <Camera style={styles.container}>
-        <View>
-          <Button icon="camera" mode="contained">
-            Photo
-          </Button>
-        </View>
-      </Camera>
-    </SafeAreaView>
+    <Camera style={styles.container} ref={cameraRef}>
+      <View style={styles.buttonContainer}>
+        <Button onPress={takePic}>Take pic</Button>
+      </View>
+      <StatusBar style="auto" />
+    </Camera>
   );
 };
 
@@ -52,7 +92,13 @@ export default CameraRoll;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  buttonContainer: {
+    backgroundColor: "#fff",
+    alignSelf: "flex-end",
+  },
+  preview: {
+    alignSelf: "stretch",
+    flex: 1,
   },
 });
